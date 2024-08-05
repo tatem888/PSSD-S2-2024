4165eca223871edcfe1cc0a90b848cc772696f73
Tate Manning <tatemanning@icloud.com>
2024-08-05 23:20:39 +0930

    sasasa

--- week1/SquareOfDigits/SquareOfDigits.cpp
+++ week1/SquareOfDigits/SquareOfDigits.cpp
@@ -16,16 +16,16 @@ class SquareOfDigits {
         
         while (currentSize >= 1)
         {
-            for (int i = 0; i < stringSizeX; i++)
+            for (int i = 0; i <= stringSizeX - currentSize; i++)
             {
-                for (int j = 0; j < stringSizeY; j++)
+                for (int j = 0; j <= stringSizeY - currentSize; j++)
                 {
                     int corner1 = data[i][j];
                     int corner2 = data[i][j + currentSize - 1];
                     int corner3 = data[i + currentSize - 1][j];
                     int corner4 = data[i + currentSize - 1][j + currentSize - 1];
 
-                    if (corner1 == corner2 && corner3 && corner4)
+                    if (corner1 == corner2 && corner1 == corner3 && corner1 == corner4)
                     {
                         return currentSize*currentSize;
                     }
@@ -37,22 +37,14 @@ class SquareOfDigits {
         return 1;
         //check if valid square
         
-        
-        
-
-         
-
-        
-        return stringSizeX;
     }
 };
 
 
-/* int main(){
+int main(){
 
     SquareOfDigits a;
 
     std::cout << a.getMax({"9785409507", "2055103694","0861396761","3073207669","1233049493","2300248968","9769239548","7984130001","1670020095","8894239889","4053971072"});
 
 }
-*/
